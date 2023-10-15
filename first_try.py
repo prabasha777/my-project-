0ec410eadb1828b535fac232d5556b5e85441f13
prabasha777 <147972480+prabasha777@users.noreply.github.com>
2023-10-15 16:18:00 +0530

    Create first_try.py

---
+++ first_try.py
@@ -0,0 +1,16 @@
+# first_try.py
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = "Army"
+    print(greet(name))
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+if __name__ == "__main__":
+    name = "Army"
+    print(greet(name))
+    print(farewell(name))
+
