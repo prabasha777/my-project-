1bb04d6982358ebe23b29b1aae49babb7bff7fc7
prabasha777 <147972480+prabasha777@users.noreply.github.com>
2023-10-15 16:20:53 +0530

    Update first_try.py
    Add welcome function

--- first_try.py
+++ first_try.py
@@ -1,10 +1,7 @@
 # first_try.py
+
 def greet(name):
     return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    name = "Army"
-    print(greet(name))
 
 def farewell(name):
     return f"Goodbye, {name}!"
